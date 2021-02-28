9257bcc2d5415f7e23813448d53c759cf1227775
infoayoub <infoayoub@github.com>
2021-02-28 20:25:26 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace _6._12.TreeView__ListView_et_Splitter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(231, 153);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // listView1
+            // 
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(388, 151);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.listView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(231, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(390, 153);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(621, 153);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.treeView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6._12.TreeView__ListView_et_Splitter
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            ImageList imageList = new ImageList();
+            imageList.Images.Add(Image.FromFile(@"C:\Users\Devman\Downloads\folder.png"));
+            imageList.Images.Add(Image.FromFile(@"C:\Users\Devman\Downloads\multimedia.png"));
+            treeView1.ImageList = imageList;
+            listView1.SmallImageList = imageList;
+            //  DirectoryInfo info = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.Desktop)); if i want to get SubDirectory of Descktop it will take a long time to get ready
+            DirectoryInfo info = new DirectoryInfo(@"C:\Users\Devman\Desktop\Package\GTA Sanandreas");
+            if (Directory.Exists(info.FullName))
+            {
+                TreeNode DirectoryNode = new TreeNode(info.Name, 0, 0);
+                treeView1.Nodes.Add(DirectoryNode);
+                LoadsubDirectory(info, DirectoryNode);
+            }
+            else MessageBox.Show("This path doesn't exists");
+
+           
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            listView1.Items.Clear();            
+            string sub_directory = @"C:\Users\Devman\Desktop\Package\"+treeView1.SelectedNode.FullPath;
+            DirectoryInfo SubDir = new DirectoryInfo(sub_directory);
+            LoadFiles(SubDir);
+        }
+
+        private void LoadsubDirectory(DirectoryInfo path, TreeNode treeNode)
+        {
+
+            string[] subdirectoryEntries = Directory.GetDirectories(path.FullName);
+        
+                // loop into subDirectory
+                foreach (string subdirctory in subdirectoryEntries)
+                {
+                    DirectoryInfo Sub_Di = new DirectoryInfo(subdirctory);
+                    TreeNode DirectoryNode = treeNode.Nodes.Add(Sub_Di.Name);
+                    LoadsubDirectory(Sub_Di, DirectoryNode);
+
+
+                }
+        }
+
+
+
+        private void LoadFiles(DirectoryInfo dir)
+        {
+            listView1.View = View.List;
+            listView1.Columns.Add("None", listView1.Width / 2);
+            // i have to creat a coloum even if i made the HeaderStyle as a none else that the listView will not show anything
+            listView1.HeaderStyle = ColumnHeaderStyle.None;// i made this one for that will not show HeaderColoumn
+
+            string[] Files = Directory.GetFiles(dir.FullName, "*.*");
+            if (Files.Length > 0)
+            {
+
+                // Loop through them to see files  
+                foreach (string file in Files)
+                {
+                        FileInfo fi = new FileInfo(file);
+                        ListViewItem item1 = new ListViewItem(fi.Name, 1);
+                        listView1.Items.Add(item1);
+                }
+            }
+        }
+    }
+}
